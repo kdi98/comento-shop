bef9493a680be1acbc10618e115deeafb07a5e03
kdi98 <dkw12560@gmail.com>
2023-01-19 16:58:00 +0900

    feat: 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      프로젝트
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navigation from './component/Navigation';
+
+function App() {
+    return (
+        <div className="App">
+            <Navigation />
+            ÄÚ¸àÅä ¼îÇÎ
+        </div>
+    );
+}
+
+export default App;
